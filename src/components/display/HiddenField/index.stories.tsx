a1f2907f19ba2c98967feed50e5c163d7fee719c
Eduardo Lorenzo <ejlorenzo19@gmail.com>
2024-03-22 21:29:17 +0100

    Moving HiddenField to Display

--- src/components/display/HiddenField/index.stories.tsx
+++ src/components/display/HiddenField/index.stories.tsx
@@ -2,7 +2,7 @@ import { HiddenField } from './HiddenField';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 export default {
-  title: 'Entry/HiddenField',
+  title: 'Display/HiddenField',
   component: HiddenField,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
